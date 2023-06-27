d63a3fe8596eb01a7b0e2d0d5f635b06af78ac53
Ronak Dinesh <ronakhq@gmail.com>
2023-06-27 12:08:00 +0400

    changes

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -163,8 +163,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>KPMG MULTI DATABASE</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to anwer your questions from GRCS, RISK and HR</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
